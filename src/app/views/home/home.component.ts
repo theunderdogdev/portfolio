d7591781910082716040959db9eea5cee8884763
Ruturaj Kalshetti <ruturajk2404@gmail.com>
2023-12-15 13:34:55 +0530

    :P Home page redesign

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  OnInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,11 +12,39 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  funPrank(){
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('dialog') dialog: ElementRef<HTMLDialogElement> | null = null;
+  videoEnded: boolean = false;
+  funPrank(evt: Event) {
+    evt.preventDefault();
+    setTimeout(() => {
+      this.dialog?.nativeElement.showModal();
+      const video = this.dialog?.nativeElement.querySelector('video');
+      if (video) {
+        video.currentTime = 0.0;
+        video.muted = false;
+        video.play().catch((err)=>{
+          console.clear();
+          console.log(err);
+          this.dialog?.nativeElement.close();
+
+        });
+        video.addEventListener('ended', () => {
+          video.pause()
+          this.videoEnded = true;
+        });
+        // document.removeChild()
+      }
+    }, 5000);
     //Todo: Link it to Never Gonna give you up with a timeout
   }
-  
+  ngAfterViewInit(): void {
+    // console.log(this.dialog?.nativeElement.showModal());
+  }
+  closeModal() {
+    this.videoEnded = false;
+    this.dialog?.nativeElement.close();
+  }
 }
