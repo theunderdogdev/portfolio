4c62667fe476dcbf50634cbdbd529d73d73fb152
Ruturaj Kalshetti <ruturajk2404@gmail.com>
2023-12-19 15:53:15 +0530

    :T Home page redesign :P Project page started

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectService {
-
-  constructor(private _http: HttpClient) {
-
-   }
-   getRepos(){
-    return this._http.get('');
-   }
+  constructor(private _http: HttpClient) {}
+  getRepos<T>() {
+    return this._http.get<T>(
+      'https://api.github.com/users/theunderdogdev/repos'
+    );
+  }
+  extractOnly<T>(data: Record<string, any>[], keys: string[]) {
+    return data.reduce((repos: Array<T>, repoData) => {
+      const repo: T = {} as T;
+      keys.forEach((key) => {
+        repo[key as keyof T] = repoData[key];
+      });
+      repos.push(repo);
+      return repos;
+    }, []);
+  }
 }
+export type Repo = {
+  id: number;
+  commits_url: string;
+  topics: string[];
+  updated_at: string;
+  url: string;
+};
--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProjectService } from '../../services/project.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit, OnDestroy {
+  projects: Project[] = [];
 
+  currId: number = 0;
+  private subs: Subscription[] = []
+  constructor(private _projService: ProjectService) {}
+  getProject(id: number, href: string) {
+    this.currId = id;
+    console.log(id, href);
+  }
+  ngOnInit(): void {
+    this.subs.push(
+    this._projService.getRepos<Record<string, any>[]>().subscribe((data) => {
+      this.projects = this._projService.extractOnly<Project>(data, [
+        'id',
+        'topics',
+        'updated_at',
+        'html_url',
+        'description',
+        'language',
+        'stargazers_count',
+        'name',
+      ]);
+      this.currId = this.projects[0].id;
+    }));
+  }
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => {
+      sub.unsubscribe();
+    })
+  }
 }
+export type Project = {
+  id: number;
+  topics: string[];
+  updated_at: string;
+  html_url: string;
+  decription: string;
+  language: string;
+  stargazers_count: number;
+  name: string;
+};
