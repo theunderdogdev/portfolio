b1f1b19fe773998f59928a924917cc1349b14517
Ruturaj Kalshetti <ruturajk2404@gmail.com>
2023-12-20 11:56:45 +0530

    :P Project page - added hover effect

--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -14,32 +14,61 @@ export class ProjectsComponent implements OnInit, OnDestroy {
   projects: Project[] = [];
 
   currId: number = 0;
-  private subs: Subscription[] = []
+  private subs: Subscription[] = [];
   constructor(private _projService: ProjectService) {}
-  getProject(id: number, href: string) {
-    this.currId = id;
-    console.log(id, href);
+  getProject(evt: Event) {
+    evt.stopPropagation();
+    const el = <HTMLButtonElement>evt.currentTarget;
+    el.scrollIntoView({ behavior: 'smooth', inline: 'center' });
+    const name = el.getAttribute('data-name');
+    const id = el.getAttribute('data-id');
+    console.log(name, id);
+    if (id !== null) {
+      this.currId = parseInt(id);
+    }
   }
   ngOnInit(): void {
     this.subs.push(
-    this._projService.getRepos<Record<string, any>[]>().subscribe((data) => {
-      this.projects = this._projService.extractOnly<Project>(data, [
-        'id',
-        'topics',
-        'updated_at',
-        'html_url',
-        'description',
-        'language',
-        'stargazers_count',
-        'name',
-      ]);
-      this.currId = this.projects[0].id;
-    }));
+      this._projService.getRepos<Record<string, any>[]>().subscribe((data) => {
+        console.log(data[0]);
+        this.projects = this._projService.extractOnly<Project>(data, [
+          'id',
+          'topics',
+          'updated_at',
+          'html_url',
+          'description',
+          'language',
+          'stargazers_count',
+          'name',
+        ]);
+        this.currId = this.projects[0].id;
+        console.log(this.projects[0]);
+      })
+    );
+    this.setWidth();
+    window.addEventListener('resize', this.setWidth);
+  }
+  setWidth(){
+    const el = document.querySelector<HTMLDivElement>('.project-detail');
+      console.log(el)
+      if (el) {
+        const maxR = Math.max(el.clientWidth, el.clientHeight);
+        el.style.setProperty('--width', `${maxR}px`);
+      }
+  }
+  setProps(evt: MouseEvent) {
+    const el = <HTMLDivElement>evt.currentTarget;
+    console.log(evt.clientX - el.clientLeft);
+    const xPos = evt.clientX - el.clientLeft;
+    const yPos = evt.clientY - el.clientTop;
+    el.style.setProperty('--x', xPos + 'px');
+    el.style.setProperty('--y', yPos + 'px');
   }
   ngOnDestroy(): void {
-    this.subs.forEach(sub => {
+    this.subs.forEach((sub) => {
       sub.unsubscribe();
-    })
+    });
+    
   }
 }
 export type Project = {
